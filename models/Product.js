42ad256f3a1346b9a3bd43b2bfb199ef9efb962e
Stephen Obie <sobie31@outlook.com>
2021-06-27 21:20:01 -0400

    added to category.js & product.js

--- models/Product.js
+++ models/Product.js
@@ -9,8 +9,34 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
   },
+  product_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  product_price: {
+    type: DataTypes.DECIMAL(10,2),
+    allowNull: true
+  },
+  product_stock: {
+    type: DataTypes.INTEGER,
+    allowNull: true
+  },
+   category_id: {
+     type: DataTypes.INTEGER,
+     references: {
+       model: 'category',
+       key: 'id',
+       unique: false
+     }
+   }
+  },
+
   {
     sequelize,
     timestamps: false,
