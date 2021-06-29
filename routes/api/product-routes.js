59068c8b8ba835143402a8863892bab1490be34b
Stephen Obie <sobie31@outlook.com>
2021-06-28 20:42:37 -0400

    updated product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,19 +4,28 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({include: [{model: Category, Tag}]});
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+    const productIdData = await Product.findByPk(req.params.id, {include: [{model: Category, Tag}]});
+    res.status(200).json(productIdData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
 router.post('/', (req, res) => {
+
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -90,6 +99,16 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
+  try {
+    const deleteProductData = await Product.destroy({
+      where: {
+        id: req.params.id
+      },
+    });
+    res.status(200).json(deleteProductData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // delete one product by its `id` value
 });
 
